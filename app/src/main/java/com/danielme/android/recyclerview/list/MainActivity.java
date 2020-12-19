92ccb03d8921d3c8ccf122bfeb4c896717aa6d60
dani <danielme_com@yahoo.com>
2020-12-19 13:56:08 +0100

    refactorizar

--- app/src/main/java/com/danielme/android/recyclerview/list/MainActivity.java
+++ app/src/main/java/com/danielme/android/recyclerview/list/MainActivity.java
@@ -1,11 +1,13 @@
 package com.danielme.android.recyclerview.list;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.widget.Toast;
 
@@ -17,53 +19,58 @@ import java.util.List;
  */
 public class MainActivity extends AppCompatActivity {
 
-    private List<Color> colors;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    setupToolbar();
 
-        initColors();
+    setupRecyclerView();
+  }
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setAdapter(new MaterialPaletteAdapter(colors, new RecyclerViewOnItemClickListener() {
-            @Override
-            public void onClick(View v, int position) {
-                Toast toast = Toast.makeText(MainActivity.this, String.valueOf(position), Toast.LENGTH_SHORT);
-                int color = android.graphics.Color.parseColor(colors.get(position).getHex());
-                toast.getView().setBackgroundColor(color);
-                toast.show();
-            }
-        }));
-        //VERTICAL
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        //HORIZONTAL
-        //recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+  private void setupToolbar() {
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+  }
 
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
-                ((LinearLayoutManager) recyclerView.getLayoutManager()).getOrientation());
-        recyclerView.addItemDecoration(dividerItemDecoration);
-    }
+  private void setupRecyclerView() {
+    RecyclerView recyclerView = findViewById(R.id.recyclerView);
+    final List<Color> colors = buildColors();
+    recyclerView.setAdapter(new MaterialPaletteAdapter(colors, new RecyclerViewOnItemClickListener() {
+      @Override
+      public void onClick(View v, int position) {
+        String text = position + " " + colors.get(position).getName();
+        Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+      }
+    }));
+    //VERTICAL
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    //HORIZONTAL
+    //recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
-    @SuppressWarnings("ResourceType")
-    private void initColors() {
-        colors = new ArrayList<>();
-        colors.add(new Color(getString(R.string.blue), getResources().getString(R.color.blue)));
-        colors.add(new Color(getString(R.string.indigo), getResources().getString(R.color.indigo)));
-        colors.add(new Color(getString(R.string.red), getResources().getString(R.color.red)));
-        colors.add(new Color(getString(R.string.green), getResources().getString(R.color.green)));
-        colors.add(new Color(getString(R.string.orange), getResources().getString(R.color.orange)));
-        colors.add(new Color(getString(R.string.grey), getResources().getString(R.color.bluegrey)));
-        colors.add(new Color(getString(R.string.amber), getResources().getString(R.color.teal)));
-        colors.add(new Color(getString(R.string.deeppurple), getResources().getString(R.color.deeppurple)));
-        colors.add(new Color(getString(R.string.bluegrey), getResources().getString(R.color.bluegrey)));
-        colors.add(new Color(getString(R.string.yellow), getResources().getString(R.color.yellow)));
-        colors.add(new Color(getString(R.string.cyan), getResources().getString(R.color.cyan)));
-        colors.add(new Color(getString(R.string.brown), getResources().getString(R.color.brown)));
-        colors.add(new Color(getString(R.string.teal), getResources().getString(R.color.teal)));
-    }
+    DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+            ((LinearLayoutManager) recyclerView.getLayoutManager()).getOrientation());
+    recyclerView.addItemDecoration(dividerItemDecoration);
+  }
+
+  @SuppressWarnings("ResourceType")
+  private List<Color> buildColors() {
+    List<Color> colors = new ArrayList<>();
+    colors.add(new Color(getString(R.string.blue), getResources().getString(R.color.blue)));
+    colors.add(new Color(getString(R.string.indigo), getResources().getString(R.color.indigo)));
+    colors.add(new Color(getString(R.string.red), getResources().getString(R.color.red)));
+    colors.add(new Color(getString(R.string.green), getResources().getString(R.color.green)));
+    colors.add(new Color(getString(R.string.orange), getResources().getString(R.color.orange)));
+    colors.add(new Color(getString(R.string.grey), getResources().getString(R.color.bluegrey)));
+    colors.add(new Color(getString(R.string.amber), getResources().getString(R.color.teal)));
+    colors.add(new Color(getString(R.string.deeppurple), getResources().getString(R.color.deeppurple)));
+    colors.add(new Color(getString(R.string.bluegrey), getResources().getString(R.color.bluegrey)));
+    colors.add(new Color(getString(R.string.yellow), getResources().getString(R.color.yellow)));
+    colors.add(new Color(getString(R.string.cyan), getResources().getString(R.color.cyan)));
+    colors.add(new Color(getString(R.string.brown), getResources().getString(R.color.brown)));
+    colors.add(new Color(getString(R.string.teal), getResources().getString(R.color.teal)));
+    return colors;
+  }
 
 }
