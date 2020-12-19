e981f71b336d5174d1bc5ef3417527e026940f61
dani <danielme_com@yahoo.com>
2020-12-19 13:11:39 +0100

    atributos finales

--- app/src/main/java/com/danielme/android/recyclerview/list/Color.java
+++ app/src/main/java/com/danielme/android/recyclerview/list/Color.java
@@ -6,28 +6,21 @@ package com.danielme.android.recyclerview.list;
  */
 public class Color {
 
-    private String name;
-    private String hex;
+  private final String name;
+  private final String hex;
+
+  public Color(String name, String hex) {
+    this.hex = hex;
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
 
 
-    public Color(String name, String hex) {
-        this.hex = hex;
-        this.name = name;
-    }
+  public String getHex() {
+    return hex;
+  }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getHex() {
-        return hex;
-    }
-
-    public void setHex(String hex) {
-        this.hex = hex;
-    }
 }
